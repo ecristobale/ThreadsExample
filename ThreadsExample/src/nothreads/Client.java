7cfbed3b527230d9e874a7f0964f21d9126273ad
Eduardo C <edu_ce_1988@hotmail.com>
2017-08-28 23:39:35 +0200

    Implemented the basic action without using Threads. Just one Thread (by default), tasks done in a sequentially way.

---
+++ ThreadsExample/src/nothreads/Client.java
@@ -0,0 +1,28 @@
+package nothreads;
+
+public class Client {
+
+	private String name;
+	private int[] shoppingCart;
+	
+	public Client(String name, int[] shoppingCart) {
+		this.name = name;
+		this.shoppingCart = shoppingCart;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int[] getShoppingCart() {
+		return shoppingCart;
+	}
+
+	public void setShoppingCart(int[] shoppingCart) {
+		this.shoppingCart = shoppingCart;
+	}
+}
